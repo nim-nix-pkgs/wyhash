d36898ff3f19605daec44219c20c641688b812b1
hugosenari <hugosenari@gmail.com>
2022-03-22 01:19:06 -0300

    chore: re-index ../nimpkgs/w/wyhash

--- 0_1_0/flake.nix
+++ 0_1_0/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-wyhash-0_1_0.flake = false;
+  inputs.src-wyhash-0_1_0.ref   = "refs/tags/0.1.0";
   inputs.src-wyhash-0_1_0.owner = "jackhftang";
-  inputs.src-wyhash-0_1_0.ref   = "0_1_0";
   inputs.src-wyhash-0_1_0.repo  = "wyhash.nim";
   inputs.src-wyhash-0_1_0.type  = "github";
   
--- main/flake.nix
+++ main/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-wyhash-main.flake = false;
+  inputs.src-wyhash-main.ref   = "refs/heads/main";
   inputs.src-wyhash-main.owner = "jackhftang";
-  inputs.src-wyhash-main.ref   = "main";
   inputs.src-wyhash-main.repo  = "wyhash.nim";
   inputs.src-wyhash-main.type  = "github";
   
